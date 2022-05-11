8eab26ec7417b1b2b92d75bce181d79ea5452205
lemoissonM <murhulametre@gmail.com>
2022-05-11 06:25:07 +0200

    Adds show or hide password

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -1,5 +1,6 @@
 import { FormControl, FormErrorMessage, FormHelperText, FormLabel, Input } from "@chakra-ui/react";
 import React from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 type PropTypes = {
     label?: string;
@@ -16,17 +17,20 @@ type PropTypes = {
 }
 
 const TextInput: React.FC<PropTypes> = ({ label, placeholder, helperText, type, id, register, error, disabled, isPassword, validation, defaultValue }) => {
+    const [showPassword, setShowPassword] = React.useState(false);
     return (
         <FormControl isInvalid={!!error}>
             <FormLabel htmlFor={id}>{label}</FormLabel>
             <Input
                 id={id}
                 placeholder={placeholder}
-                type={ isPassword ? "password" : type }
+                type={ showPassword ? 'text' : isPassword ? "password" : type }
                 disabled={disabled}
                 {...register(id, {...validation})}
                 defaultValue={defaultValue}
             />
+            { isPassword && !showPassword && <FaEye onClick={() => setShowPassword(!showPassword)} style={{position: 'absolute', right: 20, top: 45, cursor: 'pointer'}}/> }
+            { isPassword && showPassword && <FaEyeSlash onClick={() => setShowPassword(!showPassword)} style={{position: 'absolute', right: 20, top: 45, cursor: 'pointer'}}/> }
             <FormHelperText>{helperText}</FormHelperText>
             <FormErrorMessage textAlign="left">{error && error.message}</FormErrorMessage>
         </FormControl>
